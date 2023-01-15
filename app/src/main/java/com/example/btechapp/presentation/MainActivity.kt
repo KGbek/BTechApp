a16ee3fe799122a31add7814750614bad2c31d89
KGbek <edilbek@gmail.com>
2023-01-15 16:46:18 +0600

    Create project structure, adding packages.

--- app/src/main/java/com/example/btechapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/btechapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.btechapp
+package com.example.btechapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.btechapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
