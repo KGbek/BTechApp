fa8cbd8f807c0d0e59ef6eb92c46c9d1aa07aa3a
Edilbek <edilbek@gmail.com>
2023-01-27 15:58:34 +0600

    fixing bottomNavigation

--- app/src/main/java/com/example/btechapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/btechapp/presentation/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.btechapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.btechapp.R
 import com.example.btechapp.databinding.ActivityMainBinding
+import com.example.btechapp.presentation.fragments.cart.CartFragment
+import com.example.btechapp.presentation.fragments.home.HomeFragment
+import com.example.btechapp.presentation.fragments.news.NewsFragment
+import com.example.btechapp.presentation.fragments.profile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +26,35 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
-            // setup()
+        loadFragment(HomeFragment())
+        bottomNav = binding.bottomNavigation
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.navigation_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_news -> {
+                    loadFragment(NewsFragment())
+                true
+                }
+                R.id.navigation_cart -> {
+                    loadFragment(CartFragment())
+                    true
+                }
+                R.id.navigation_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
-//    private fun setup() {
-//        TODO("Not yet implemented")
-//    }
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment_activity_main, fragment)
+        transaction.commit()
+    }
+
 }
