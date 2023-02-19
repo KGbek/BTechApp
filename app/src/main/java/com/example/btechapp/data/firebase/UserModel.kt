5bb5e4dbfb4478a5f2e46f38da69bcc110610fb4
dantist987 <adik.isak2412@gmail.com>
2023-02-19 15:13:56 +0600

    Firebase added

---
+++ app/src/main/java/com/example/btechapp/data/firebase/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.btechapp.data.firebase
+
+data class User(
+    val id: String,
+    val name: String,
+    val second_name: String,
+    val email: String,
+    val password: String
+)
